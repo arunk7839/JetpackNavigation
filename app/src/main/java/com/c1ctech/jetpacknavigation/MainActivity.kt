793d0e9315580a4bd9c240c04296f68562d6f7a3
arun <arunk7839@gmail.com>
2022-04-06 13:18:42 +0530

    Initial commit

---
+++ app/src/main/java/com/c1ctech/jetpacknavigation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.c1ctech.jetpacknavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = this.findNavController(R.id.nav_host_fragment)
+
+        setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
+    }
+}
